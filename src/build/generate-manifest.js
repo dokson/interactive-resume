5b822f4b55c6eb1499252a6eac3d787e3d8944b2
Alessandro Colace <ale.colace@gmail.com>
2025-06-19 16:08:51 +0200

    fix: update icon paths in manifest generation

--- src/build/generate-manifest.js
+++ src/build/generate-manifest.js
@@ -19,7 +19,10 @@ const manifest = {
     name: `${packageJson.author} - ${packageJson.description}`,
     short_name: getShortName(packageJson.author),
     description: `${packageJson.description} of ${packageJson.author} | ${packageJson.job_title} - based on ${packageJson.based_on}`,
-    icons: packageJson.config.manifest.icons,
+    icons: packageJson.config.manifest.icons.map(icon => ({
+        ...icon,
+        src: `${packageJson.name}${packageJson.config.manifest.iconsPath}${icon.src}`
+    })),
     theme_color: packageJson.config.manifest.themeColor,
     background_color: packageJson.config.manifest.backgroundColor,
     display: packageJson.config.manifest.display,
