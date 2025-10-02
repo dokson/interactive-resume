46152b9b2cc541088639cfe867b706426ed7f092
Alessandro Colace <ale.colace@gmail.com>
2025-10-02 18:20:46 +0200

    fix: handling emailjs dependencies

--- src/build/copy-libs.js
+++ src/build/copy-libs.js
@@ -1,18 +1,35 @@
 const fs = require('fs');
 const path = require('path');
 
-const packageJson = require(path.join(__dirname, '../../package.json'));
+const rootDir = path.join(__dirname, '../../');
+const packageJson = require(path.join(rootDir, 'package.json'));
+
+const libsDir = path.join(rootDir, packageJson.config.build.libsDir);
+const emailJsDir = path.join(rootDir, 'node_modules', '@emailjs', 'browser', 'dist');
+const outputDir = path.join(rootDir, packageJson.config.build.outputDir);
 
 console.log('🔄 Copy external libs...');
 
-fs.readdirSync(packageJson.config.build.libsDir).forEach(file => {
+fs.readdirSync(libsDir).forEach(file => {
     try {
-        const srcPath = path.join(packageJson.config.build.libsDir, file);
-        const destPath = path.join(packageJson.config.build.outputDir, file);
+        const srcPath = path.join(libsDir, file);
+        const destPath = path.join(outputDir, file);
         fs.copyFileSync(srcPath, destPath);
-        console.log(`✅ Copied to ${destPath}`);
+        console.log(`✅ Copied ${file} to ${outputDir}`);
     } catch (error) {
-        console.error(`❌ Error copying ${file}: ${error.message}`);
+        console.error(`❌ Error copying ${file} from libsDir: ${error.message}`);
         process.exit(1);
     }
 });
+
+try {
+    emailJsLibFile = 'email.min.js'
+    const emailjsSrcPath = path.join(emailJsDir, emailJsLibFile);
+    const emailjsDestPath = path.join(outputDir, emailJsLibFile);
+
+    fs.copyFileSync(emailjsSrcPath, emailjsDestPath);
+    console.log(`✅ Copied ${emailJsLibFile} from node_modules to ${outputDir}`);
+} catch (error) {
+    console.error(`❌ Error copying email.min.js from node_modules: ${error.message}`);
+    process.exit(1);
+}
--- src/js/mail.js
+++ src/js/mail.js
@@ -1,33 +1,33 @@
 emailjs.init("H_cQjD2uFvh4WSAUf");
 
 function initContactButton() {
-    "computer" == deviceName ? 
-        sendEmailDiv.onclick = function () { sendEmail() } : 
+    "computer" == deviceName ?
+        sendEmailDiv.onclick = function () { sendEmail() } :
         sendEmailDiv.addEventListener("touchstart", sendEmail, !1);
 }
 
 function sendEmail() {
     hideContactConfirmationContainer();
     positionContactConfirmationContainer();
-    
-    var a, b, 
-        c = $("#email-address").val(), 
-        d = $("#email-subject").val(), 
+
+    var a, b,
+        c = $("#email-address").val(),
+        d = $("#email-subject").val(),
         e = $("#email-message").val();
-    
+
     if (c.match(/^([a-z0-9._-]+@[a-z0-9._-]+\.[a-z]{2,}$)/i)) {
-        if (e.length < 1 && (b = !1, focusMessage()), 
-            e.length >= 1 && (b = !0), 
-            d.length < 1 && (a = !1, focusSubject()), 
-            d.length >= 1 && (a = !0), 
+        if (e.length < 1 && (b = !1, focusMessage()),
+            e.length >= 1 && (b = !0),
+            d.length < 1 && (a = !1, focusSubject()),
+            d.length >= 1 && (a = !0),
             1 == a && 1 == b) {
-            
+
             var f = {
                 "from_email": c,
                 "subject": d,
                 "message": e
             };
-            
+
             setTimeout("showContactConfirmationContainer(2)", 200);
             setTimeout(function () { send(f) }, 2e3);
         } else {
@@ -42,14 +42,14 @@ function sendEmail() {
 
 function send(templateParams) {
     emailjs.send('service_9u4crjr', 'template_hfnne2s', templateParams)
-        .then(function(response) {
+        .then(function (response) {
             console.log('Email sent successfully!', response.status, response.text);
             hideContactConfirmationContainer();
             positionContactConfirmationContainer();
             setTimeout("showContactConfirmationContainer(4)", 200);
             clearAllInputField();
         })
-        .catch(function(error) {
+        .catch(function (error) {
             console.log('Email failed to send:', error);
             hideContactConfirmationContainer();
             positionContactConfirmationContainer();
